db167c0bbd262f559e657fedd53bb6dde57442c6
UpinBen <30612894+BenHavis@users.noreply.github.com>
2025-08-14 16:45:16 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Analytics } from "@vercel/analytics/next"
 import Link from 'next/link';
 
 export const metadata: Metadata = {
@@ -49,6 +50,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </div>
         </header>
         <main>{children}</main>
+          <Analytics />
       </body>
     </html>
   );

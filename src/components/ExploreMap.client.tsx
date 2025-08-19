b52f33f80dacc5d1a46aedb4800d9afd601e3ae4
UpinBen <30612894+BenHavis@users.noreply.github.com>
2025-08-19 15:33:08 -0400

    update marker

--- src/components/ExploreMap.client.tsx
+++ src/components/ExploreMap.client.tsx
@@ -12,6 +12,15 @@ import 'leaflet/dist/leaflet.css';
 import { LatLngTuple } from 'leaflet';
 import styles from '@/app/explore/explore.module.css';
 
+import L from 'leaflet';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 function RecenterOnLocation({ coords }: { coords: LatLngTuple }) {
   const map = useMap();
   useEffect(() => {
